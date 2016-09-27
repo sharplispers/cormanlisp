55d47965e166fb6616efbe1796d64e5130211b0b
Artem Boldarev <artem.boldarev@gmail.com>
2016-09-27 15:55:13 +0300

    Add project to build CormanLisp image from Visual Studio.

---
+++ delimg.bat
@@ -0,0 +1,1 @@
+del "%~dp0\CormanLisp.img"
--- makeimg.bat
+++ makeimg.bat
@@ -1,2 +1,7 @@
-del cormanlisp.img
-clconsole -execute sys/compile-sys.lisp -image ""
+set OldDir=%cd%
+
+cd "%~dp0\"
+del "%~dp0\CormanLisp.img"
+"%~dp0\clconsole" -execute "%~dp0\sys\compile-sys.lisp" -image ""
+
+cd "%OldDir%"
