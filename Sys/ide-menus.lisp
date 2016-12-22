44e6c6fe4a90bbb663b8591fc3c56a4e25ed8b5a
Artem Boldarev <artem.boldarev@gmail.com>
2016-12-22 18:28:44 +0200

    Update IDE menus after image loading.

--- Sys/ide-menus.lisp
+++ Sys/ide-menus.lisp
@@ -9,8 +9,21 @@
 
 (in-package :ide)
 
+(defun menu-exists-p (menu-name &optional submenu-name)
+    (let ((menu (win::find-named-menu (win::get-main-menu) menu-name)))
+        (if (not submenu-name)
+            (when menu t)
+            (progn
+                (when (win::find-named-menu menu submenu-name)
+                    t)))))
+
 (defun setup-ide-menus ()
     ;; create dynamic Package submenu (under Lisp menu)
+    ;; remove old ones
+    (win:remove-menu-item "&Lisp" 5) ; "&Zoom 1 to 1" 
+    (win:remove-menu-item "&Lisp" 4) ; "&Top Level"
+    (win:remove-menu-item "&Lisp" 3) ; "Package"
+
 	(win:create-dynamic-menu-item 
 		(list :menu "Package"
             (lambda (hmenu)
@@ -48,10 +61,22 @@
 		    5
             :message "Set edit window zoom to 1 to 1")
     
+    ;; remove old item
+    (when (menu-exists-p "H&istory")
+        (win:remove-menu-item nil 6))
+    
+    (when (menu-exists-p "&Symbols")
+        (win:remove-menu-item nil 5))
+    
+    (when (menu-exists-p "&Declarations")
+        (win:remove-menu-item nil 4))
+    
     (ide:add-jumpmenu-to-menubar)
     (ide:add-symbol-menu-to-menubar)
     (ide:add-command-history-menu-to-menubar)
     
+    ;; remove old item 
+    (win:remove-menu-item "&Help" 4)
     (win:create-menu-item 
 		(list :command "Corman Lisp &Forums..." 
 			(lambda (x)
