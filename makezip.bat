224ffa455087207f846b0e391491bda81816476e
Artem Boldariev <artem.boldarev@gmail.com>
2019-01-02 23:04:03 +0200

    makezip.bat fixes.

--- makezip.bat
+++ makezip.bat
@@ -1,8 +1,8 @@
 @echo off
-rem 
+rem Repack the installer contents into a ZIP archive.
 
 mkdir .\unpacked
-msiexec /a CormanLispInstaller.msi /qb TARGETDIR="%~dp0unpacked"
+msiexec /a CormanLispInstaller.msi /qn TARGETDIR="%~dp0unpacked"
 del CormanLispBinaries.zip
 powershell Compress-Archive -Path \"%~dp0unpacked\Corman Lisp\" -DestinationPath \"%~dp0CormanLispBinaries.zip\"
 rd /s /q .\unpacked
